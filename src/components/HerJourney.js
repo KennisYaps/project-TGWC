54ab68ca3980753ebd4adf2157645fee684a3d55
yyx <yuan_xin96@hotmail.com>
2018-03-27 16:51:09 +0800

    added seed data in JourneySeedData

--- src/components/HerJourney.js
+++ src/components/HerJourney.js
@@ -1,8 +1,8 @@
 import React from "react";
-
 const HerJourney = () => (
   <div>
-    <p>You are in Her Journey page</p>
+    <h1>Her Journey</h1>
+
   </div>
 );
 
---
+++ src/utils/JourneySeedData.js
@@ -0,0 +1,22 @@
+export const JourneyDetailsArray = [
+  {
+    title: "Day 1",
+    text:
+      "Day 1. React components implement a render() method that takes input data and returns what to display. This example uses an XML-like syntax called JSX. Input data that is passed into the component can be accessed by render() via this.props. "
+  },
+  {
+    title: "Day 2",
+    text:
+      "Day 2. A Stateful Component. In addition to taking input data (accessed via this.props), a component can maintain internal state data (accessed via this.state). When a component’s state data changes, the rendered markup will be updated by re-invoking render()."
+  },
+  {
+    title: "Day 3",
+    text:
+      "Day 3. An Application. Using props and state, we can put together a small Todo application. This example uses state to track the current list of items as well as the text that the user has entered. Although event handlers appear to be rendered inline, they will be collected and implemented using event delegation."
+  },
+  {
+    title: "Day 4",
+    text:
+      "Day 4. External Plugins. React is flexible and provides hooks that allow you to interface with other libraries and frameworks. This example uses remarkable, an external Markdown library, to convert the <textarea>’s value in real time."
+  }
+];
