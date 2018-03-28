54b81b7644d722227582f430d6e6f3c95997c856
yappps <yapyuanxin@gmail.com>
2018-03-28 16:43:27 +0800

    deleted journey seed data

--- src/components/HerJourney.js
+++ src/components/HerJourney.js
@@ -1,27 +1,37 @@
 import React, { Component } from "react";
-import { TimelinesArray } from "../utils/JourneysSeedData";
-import Timeline from './Timeline';
+import Timeline from "./Timeline";
 class HerJourney extends Component {
   constructor() {
     super();
     this.state = {
-      timelines: TimelinesArray
+      timelines: []
     };
   }
+
   render() {
     const displayTimelines = this.state.timelines.map(
       (timeline, timelineIndex) => {
-        return (
-          <Timeline key={timelineIndex} timelineDatas={timeline}/>
-        );
+        return <Timeline key={timelineIndex} timelineDatas={timeline} />;
       }
     );
     return (
       <div>
         <h1>Her Journey</h1>
         <div id="timelines">{displayTimelines}</div>
+  
       </div>
     );
+  }
+  componentDidMount() {
+    fetch("http://localhost:3000/timelines")
+      .then(data => {
+        return data.json();
+      })
+      .then(data =>
+        this.setState({
+          timelines: data
+        })
+      );
   }
 }
 export default HerJourney;
