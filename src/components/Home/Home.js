72b45cb8c67cf7809f51566ca3e94ecb0b3de471
yappps <yapyuanxin@gmail.com>
2018-04-01 17:26:39 +0800

    added nav bar andhome page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,29 @@
 import React from "react";
-import '../../CSS/style.css'
-
+import '../../CSS/Home.css';
 const Home = () => (
-  <div>
-  <h1 id="TGWC">The girl who code</h1>
+  <div id="Home">
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About Her</a>
+        </li>
+        <li>
+          <a href="#">Her Journey</a>
+        </li>
+        <li>
+          <a href="#">Her Mistakes</a>
+        </li>
+        <li>
+          <a href="#">Her Projects</a>
+        </li>
+      </ul>
+    </nav>
+    <section className="background-image">
+      <h1 id="TGWC-title">The girl who code</h1>
+    </section>
   </div>
 );
 
