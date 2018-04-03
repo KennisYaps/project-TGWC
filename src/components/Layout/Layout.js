add054865f3cb8a2e8199536c742d144e2437019
yappps <yapyuanxin@gmail.com>
2018-04-03 19:32:28 +0800

    deleted spacing in components folder naming

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Home from "../Home/Home";
-import HerWorks from "../Her Works/HerWorks";
-import HerStory from "../Her Story/HerStory";
-import HerSkills from "../Her Skills/HerSkills";
-import AboutHer from "../About Her/AboutHer";
-import HerMistakes from "../Her Mistakes/HerMistakes";
+import HerWorks from "../HerWorks/HerWorks";
+import HerStory from "../HerStory/HerStory";
+import HerSkills from "../HerSkills/HerSkills";
+import AboutHer from "../AboutHer/AboutHer";
+import HerMistakes from "../HerMistakes/HerMistakes";
 
 const Layout = () => (
   <div id="Layout">
