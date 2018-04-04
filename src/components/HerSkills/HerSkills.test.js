09db00be25b7255ad06aa1d2de6241c9f8a68efd
yappps <yapyuanxin@gmail.com>
2018-04-04 21:44:14 +0800

    added a test case for HerSkills Component

--- src/components/HerSkills/HerSkills.test.js
+++ src/components/HerSkills/HerSkills.test.js
@@ -4,7 +4,10 @@ import SkillsChart from "./SkillsChart";
 import HerSkills from "./HerSkills";
 
 describe("Her Skills", () => {
-
+  it("should have 1 <h1> tag", () => {
+    const wrapper = shallow(<HerSkills />);
+    expect(wrapper.find("h1")).toHaveLength(1);
+  });
   it("should have 1 <SkillsChart/> component", () => {
     const wrapper = shallow(<HerSkills />);
     expect(wrapper.find(SkillsChart)).toHaveLength(1);
