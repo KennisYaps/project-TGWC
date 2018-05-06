ea215a1d0a622f85e620ae83e69a7c0d7e4e549b
yappps <yapyuanxin@gmail.com>
2018-05-06 22:56:59 +0800

    added another project

--- src/components/HerWorks/HerWorks.js
+++ src/components/HerWorks/HerWorks.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "../../index.css";
-import './HerWorks.css';
+import "./HerWorks.css";
 const HerWorks = () => (
   <div id="HerWorks">
     <h1>HER WORKS</h1>
-    <div id="Fetch-React-Lab-container">
-      <p className="Work-Image" />
+    <div className="Project-container">
+      <p className="Work-Image-Fetch-React-Lab" />
       <div className="Work-Description">
         <h2 className="Work-Title">Fetch React Lab</h2>
         <p className="Work-Link">
@@ -13,6 +13,22 @@ const HerWorks = () => (
           <a
             className="anchor-text"
             href="https://github.com/yappps/fetch-react-lab"
+          >
+            Click To View The Source Code
+          </a>
+        </p>
+      </div>
+    </div>
+    <div className="Project-container">
+      <p className="Work-Image-myFeedback" />
+      <div className="Work-Description">
+        <h2 className="Work-Title">myFeedback App</h2>
+        <p className="Work-Link">
+          <i className="material-icons">code</i>
+          <a
+            className="anchor-text"
+            href="http://my-feedback-team-b-staging.herokuapp.com/ 
+            "
           >
             Click To View The Source Code
           </a>
