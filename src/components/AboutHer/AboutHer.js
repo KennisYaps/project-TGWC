05e21d861c12c93c7a92dedfbc326e368b32c4aa
yappps <yapyuanxin@gmail.com>
2018-05-06 22:56:35 +0800

    change text

--- src/components/AboutHer/AboutHer.js
+++ src/components/AboutHer/AboutHer.js
@@ -1,15 +1,24 @@
 import React from "react";
-import '../../index.css';
-import './AboutHer.css';
+import "../../index.css";
+import "./AboutHer.css";
 
 const AboutHer = () => (
   <div id="AboutHer">
     <h1>ABOUT HER</h1>
     <span id="about-her-text">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est fugiat
-      voluptatum, aliquid illum natus veniam nulla ducimus assumenda sed quaerat
-      obcaecati illo suscipit laudantium quidem doloribus magnam odit cum.
-      Quasi!
+      <p>
+        She is passionate about programming and is a self-taught programmer. To
+        her, programming is like a superpower in today’s world. It allows her to
+        transform pure thoughts into something tangible and useful that solves
+        real-world problems. It gives her great satisfaction to know the
+        positive impact to society that she can have through building software
+        programs.
+      </p>
+      <p>
+        Her dream is to become a cybersecurity specialist who plays a proactive
+        role in shaping and improving the way the world secure their
+        information.
+      </p>
     </span>
   </div>
 );
